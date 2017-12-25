1ea52a9d091a1b3c617372f671f5330f38c33ffe
zhenglong <long.zheng@mtime.com>
2017-12-25 23:28:02 +0800

    update my-cloud-config-service

--- my-cloud-config-service/src/main/java/org/my/cloud/eureka/ConfigManagerApplication.java
+++ my-cloud-config-service/src/main/java/org/my/cloud/eureka/ConfigManagerApplication.java
@@ -2,12 +2,14 @@ package org.my.cloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by Administrator on 2017/12/19.
  */
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigManagerApplication.class, args);
